ef3073ae698e1642da0a5f58c2a91ce1068cdfb4
xxx-1203 <84116351+xxx-1203@users.noreply.github.com>
2022-01-10 15:32:36 +0900

    Add files via upload

--- day3/src/text/class1/RunTV.java
+++ day3/src/text/class1/RunTV.java
@@ -7,20 +7,20 @@ public class RunTV {
       TV tv2 = new TV();
       TV tv3 = new TV();
       
-      tv1.setProducer("LG");
+     // tv1.setProducer("LG");
       tv1.setColor("BLACK");
       tv1.setSize(55);
       
-      tv2.setProducer("SAMSUNG");
+     // tv2.setProducer("SAMSUNG");
       tv2.setColor("WHITE");
       tv2.setSize(77);
       
-      tv3.setProducer("APPLE");
+     // tv3.setProducer("APPLE");
       tv3.setColor("METAL");
       tv3.setSize(100);
       
       System.out.println("********************************");
-      System.out.println("\t"+tv1.getproducer()+"전자 TV");
+     // System.out.println("\t"+tv1.getproducer()+"전자 TV");
       System.out.println("\t"+tv1.getSize()+"인치");
       System.out.println("\t"+tv1.getColor()+"전자 TV");
       tv1.powerOn();
@@ -29,14 +29,14 @@ public class RunTV {
       }
       System.out.println("********************************");
       System.out.println("********************************");
-      System.out.println("\t"+tv2.getproducer()+"전자 TV");
+     // System.out.println("\t"+tv2.getproducer()+"전자 TV");
       System.out.println("\t"+tv2.getSize()+"인치");
       System.out.println("\t"+tv2.getColor()+"전자 TV");
       tv2.powerOn();
       for (int i=0; i<7; i++) {
     	  tv2.upVolume();
       }
-      tv2.powerOff
+     // tv2.powerOff;
       System.out.println("********************************");
    }//main
 
--- day4/src/textclass2/RunConstructorTest1.java
+++ day4/src/textclass2/RunConstructorTest1.java
@@ -6,6 +6,8 @@ public class RunConstructorTest1 {
 		ConstructorTest1 c1 = new ConstructorTest1(7);
 		ConstructorTest1 c2 = new ConstructorTest1(11);
 
+		System.out.println("c1이 참조하는 객체의 n1 필드의 값은 "+c1.getN1());
+		System.out.println("c2이 참조하는 객체의 n1 필드의 값은 "+c2.getN1());
 	}
 
 }
