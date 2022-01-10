1737d2c3703879c68e05d29c40e1bec28c7e1e08
xxx-1203 <84116351+xxx-1203@users.noreply.github.com>
2022-01-10 15:35:12 +0900

    Add files via upload

--- day6/src/test/static1/RunTV.java
+++ day6/src/test/static1/RunTV.java
@@ -5,11 +5,11 @@ public class RunTV {
 	public static void main(String[] args) {
 		TV t1 = new TV(32, "Blue");
 		TV t2 = new TV(55, "Red");
-		TV t3 = new Tv(100, "Green");
+		TV t3 = new TV(100, "Green");
 	}
-	t1.producer = "LG전자";
+	TV.producer = "LG전자";
 	
-	System.out.printf("t1이 참조하는 TV 객체의 맴버필드 color: %s, size: %d, producer: %s\n", t1.getColor(), t1.getSize(), t1.producer);
-	System.out.printf("t2이 참조하는 TV 객체의 맴버필드 color: %s, size: %d, producer: %s\n", t2.getColor(), t2.getSize(), t2.producer);
-	System.out.printf("t3이 참조하는 TV 객체의 맴버필드 color: %s, size: %d, producer: %s\n", t3.getColor(), t3.getSize(), t3.producer);
+	System.out.printf("t1이 참조하는 TV 객체의 맴버필드 color: %s, size: %d, producer: %s\n", t1.getColor(), t1.getSize(),TV.producer);
+	System.out.printf("t2이 참조하는 TV 객체의 맴버필드 color: %s, size: %d, producer: %s\n", t2.getColor(), t2.getSize(),TV.producer);
+	System.out.printf("t3이 참조하는 TV 객체의 맴버필드 color: %s, size: %d, producer: %s\n", t3.getColor(), t3.getSize(),TV.producer);
 }
